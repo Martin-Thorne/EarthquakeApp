54e5b82c6cc3b56b8ce4d62fd1b099d00b35b28d
marti <martinjthorne@gmail.com>
2019-06-20 15:57:41 +0100

    Added helper class to parse json earthquake data

---
+++ app/src/main/java/com/martint/earthquakeapp/GetParseEarthquakes.java
@@ -0,0 +1,59 @@
+package com.martint.earthquakeapp;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Helper class for getting/parsing earthquake data
+ */
+public class GetParseEarthquakes {
+
+    private GetParseEarthquakes() {
+    }
+
+    /**
+     * Parses usgs json string to extract data to create Earthquake objects
+     *
+     * @param jsonResponse
+     * @return Array of Earthquake objects
+     */
+    public static ArrayList<Earthquake> parseEarthquakes(String jsonResponse) {
+        // Empty array list to add earthquakes to
+        ArrayList<Earthquake> earthquakes = new ArrayList<>();
+        try {
+            // Create JSONObject from json string
+            JSONObject jsonObject = new JSONObject(jsonResponse);
+            // Create JSONArray of the features of each earthquake in JSONObject
+            JSONArray feature = jsonObject.getJSONArray("features");
+
+            // loop for each earthquake
+            for (int i = 0; i < feature.length(); i++) {
+                JSONObject jsonObject1 = feature.getJSONObject(i);
+                // JSONObjects hold json objects with keys properties/geometry
+                JSONObject jsonObject2 = jsonObject1.getJSONObject("properties");
+                JSONObject jsonObject3 = jsonObject1.getJSONObject("geometry");
+                // JSONArray hold json array of object geometry with key coordinates
+                JSONArray jsonArray = jsonObject3.getJSONArray("coordinates");
+                // Access JSONArray to get latitude/longitude
+                double latitude = jsonArray.getDouble(1);
+                double longitude = jsonArray.getDouble(0);
+                // Access JSONObject to get location/magnitude/url
+                String location = jsonObject2.getString("place");
+                double magnitude = jsonObject2.getDouble("mag");
+                String url = jsonObject2.getString("url");
+                earthquakes.add(new Earthquake(latitude, longitude, location, magnitude, url));
+            }
+
+
+        } catch (JSONException e) {
+            Log.e("GetParseEarthquakes", "Problem parsing JSON results", e);
+        }
+
+        return earthquakes;
+    }
+}
--- app/src/main/java/com/martint/earthquakeapp/MapsActivity.java
+++ app/src/main/java/com/martint/earthquakeapp/MapsActivity.java
@@ -18,27 +18,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+    // Temporary earthquake data used to test parsing
+    private static final String EARTHQUAKE_DATA = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1560948066000,\"url\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=5\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.8.1\",\"limit\":5,\"offset\":1,\"count\":5},\"features\":[{\"type\":\"Feature\",\"properties\":{\"mag\":6.4,\"place\":\"140km NE of L'Esperance Rock, New Zealand\",\"time\":1560927702801,\"updated\":1560935158945,\"tz\":-720,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us600042vn\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us600042vn&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":0,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":630,\"net\":\"us\",\"code\":\"600042vn\",\"ids\":\",pt19170000,at00ptc3is,us600042vn,\",\"sources\":\",pt,at,us,\",\"types\":\",geoserve,ground-failure,impact-link,losspager,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.356,\"rms\":1.51,\"gap\":51,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.4 - 140km NE of L'Esperance Rock, New Zealand\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-177.787,-30.6004,10]},\"id\":\"us600042vn\"},\n" +
+            "{\"type\":\"Feature\",\"properties\":{\"mag\":6.4,\"place\":\"31km WSW of Tsuruoka, Japan\",\"time\":1560864139013,\"updated\":1560940556302,\"tz\":540,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us600042fx\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us600042fx&format=geojson\",\"felt\":131,\"cdi\":9.1,\"mmi\":6.871,\"alert\":\"yellow\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":769,\"net\":\"us\",\"code\":\"600042fx\",\"ids\":\",us600042fx,pt19169001,at00ptaqha,\",\"sources\":\",us,pt,at,\",\"types\":\",dyfi,geoserve,ground-failure,impact-link,losspager,moment-tensor,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.295,\"rms\":0.83,\"gap\":33,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.4 - 31km WSW of Tsuruoka, Japan\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[139.4723,38.646,12]},\"id\":\"us600042fx\"},\n" +
+            "{\"type\":\"Feature\",\"properties\":{\"mag\":6,\"place\":\"135km ENE of L'Esperance Rock, New Zealand\",\"time\":1560751325750,\"updated\":1560837961875,\"tz\":-720,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us600041lv\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us600041lv&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":0,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":554,\"net\":\"us\",\"code\":\"600041lv\",\"ids\":\",us600041lv,\",\"sources\":\",us,\",\"types\":\",geoserve,ground-failure,losspager,moment-tensor,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.711,\"rms\":1.15,\"gap\":49,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.0 - 135km ENE of L'Esperance Rock, New Zealand\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-177.5972,-30.9381,16]},\"id\":\"us600041lv\"},\n" +
+            "{\"type\":\"Feature\",\"properties\":{\"mag\":6.3,\"place\":\"93km ENE of L'Esperance Rock, New Zealand\",\"time\":1560662236749,\"updated\":1560774595040,\"tz\":-720,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us600041b3\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us600041b3&format=geojson\",\"felt\":2,\"cdi\":2,\"mmi\":0,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":611,\"net\":\"us\",\"code\":\"600041b3\",\"ids\":\",us600041b3,pt19167000,at00pt6eof,\",\"sources\":\",us,pt,at,\",\"types\":\",dyfi,geoserve,ground-failure,impact-link,losspager,moment-tensor,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.812,\"rms\":1.6,\"gap\":37,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.3 - 93km ENE of L'Esperance Rock, New Zealand\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-178.0232,-31.0615,35]},\"id\":\"us600041b3\"},\n" +
+            "{\"type\":\"Feature\",\"properties\":{\"mag\":7.2,\"place\":\"103km NE of L'Esperance Rock, New Zealand\",\"time\":1560639302415,\"updated\":1560797309596,\"tz\":-720,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us6000417i\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us6000417i&format=geojson\",\"felt\":12,\"cdi\":8.3,\"mmi\":6.553,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":807,\"net\":\"us\",\"code\":\"6000417i\",\"ids\":\",at00pt5wzj,us6000417i,pt19166001,\",\"sources\":\",at,us,pt,\",\"types\":\",dyfi,finite-fault,general-text,geoserve,impact-link,losspager,moment-tensor,origin,phase-data,poster,shakemap,\",\"nst\":null,\"dmin\":1.561,\"rms\":1.14,\"gap\":23,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 7.2 - 103km NE of L'Esperance Rock, New Zealand\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-178.0945,-30.8051,34.4]},\"id\":\"us6000417i\"}],\"bbox\":[-178.0945,-31.0615,10,139.4723,38.646,35]}";
+
 
     private GoogleMap map;
-    ArrayList<Earthquake> earthquakes;
+    private ArrayList<Earthquake> earthquakes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        //Creates a set of fake earthquakes, stored in an array, to display on map
-        earthquakes = new ArrayList<>();
-        Earthquake earthquake1 = new Earthquake(-30.6004, -177.787, "140km NE of L'Esperance Rock, New Zealand", 6.4, "https://earthquake.usgs.gov/earthquakes/eventpage/us600042vn");
-        earthquakes.add(earthquake1);
-        Earthquake earthquake2 = new Earthquake(38.646, 139.4723, "31km WSW of Tsuruoka, Japan", 6.4, "https://earthquake.usgs.gov/earthquakes/eventpage/us600042fx");
-        earthquakes.add(earthquake2);
-        Earthquake earthquake3 = new Earthquake(-30.9381, -177.5972, "135km ENE of L'Esperance Rock, New Zealand", 6, "https://earthquake.usgs.gov/earthquakes/eventpage/us600041lv");
-        earthquakes.add(earthquake3);
-        Earthquake earthquake4 = new Earthquake(-31.0615, -178.0232, "93km ENE of L'Esperance Rock, New Zealand", 6.3, "https://earthquake.usgs.gov/earthquakes/eventpage/us600041b3");
-        earthquakes.add(earthquake4);
-        Earthquake earthquake5 = new Earthquake(-30.8051, -178.0945, "103km NE of L'Esperance Rock, New Zealand", 7.2, "https://earthquake.usgs.gov/earthquakes/eventpage/us6000417i");
-        earthquakes.add(earthquake5);
+        // Initialises ArrayList to parsed earthquake data
+        earthquakes = GetParseEarthquakes.parseEarthquakes(EARTHQUAKE_DATA);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
